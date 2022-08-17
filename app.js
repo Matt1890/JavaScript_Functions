c0b9939ee82871078738348181cd943351671990
“matthewhasemeier” <mshweb22@gmail.com>
2022-08-17 12:33:22 -0700

    exercise1

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
 console.log("Hello World!\n==========\n");
 
+function printOdds(){
+    let count = 10
+    while (count >= 1) {
+        if (count % 2 == 0){ 
+            count --
+        }
+        else {
+            console.log(count + " is odd") 
+        count --;
+        }
+    }
+
+}
+
+printOdds ()
+
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
 
