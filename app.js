1d094f09f9d1668cb5faee77b933a16c36fa61d4
“matthewhasemeier” <mshweb22@gmail.com>
2022-09-29 01:10:03 -0700

    exercise 1 done, 2 needs review

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 console.log("Hello World!\n==========\n");
 
+// Exercise 1 Section
+console.log("EXERCISE 1:\n==========\n");
+
 function printOdds(){
     let count = 10
     while (count >= 1) {
@@ -16,8 +19,26 @@ function printOdds(){
 
 printOdds ()
 
-// Exercise 1 Section
-console.log("EXERCISE 1:\n==========\n");
-
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
+
+function checkAge(){
+    let name = prompt("What is your name?");
+    console.log(`Greetings ${name}`);
+    let age = prompt("What is your age?");
+    for (name; age; ;){
+        if (name is >= 16){
+            console.log(`Congratulations ${name}! You are old enough to drive.`);
+        } else if(name < 16) {
+            console.log(`Sorry ${name}, you are not old enough to drive.`);
+        } else (age == NaN){
+            console.log("Invalid input.")
+        }   
+    }
+};
+
+checkAge()
+
+// Exercise 3 Section
+console.log("EXERCISE 3:\n==========\n");
+
